41273516193e6f166f1ada678636c0b5b60ff145
Vinicius Tonon <viniciustonon@gmail.com>
2020-05-11 17:40:03 -0300

    fix(db): changes to DB entrypoint (ES6)

--- ops/db_entrypoint.js
+++ ops/db_entrypoint.js
@@ -3,6 +3,7 @@ const { MongoClient } = require('mongodb')
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const dbName = 'lunchBot'
+
 client.connect((err, client) => {
     const db = client.db(dbName)
 
