6661b6966a02a22623feddaf4e79cd667d37ed31
Vinicius Tonon <viniciustonon@gmail.com>
2020-05-11 17:28:55 -0300

    chore(app): app bringup

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const { MongoClient } = require('mongodb')
+const assert = require('assert')
+const bodyParser = require('body-parser')
+const { ObjectID } = require('bson');
+
+const app = express();
+const port = 3000;
+
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'lunchBot';
+const client = new MongoClient(mongoUrl);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function getBots(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+
+        db.collection('bots').find({}).toArray((err, docs) => {
+            assert.equal(null, err);
+            res.send(docs)
+        })
+    })
+};
+
+function getBotById(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        
+        db.collection('bots').find(
+            {id:req.params.id}
+            ).toArray((err, docs) => {
+                assert.equal(null, err);
+                res.send(docs);
+        });
+    });
+};
+
+function postMessage(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client(dbName);
+
+        let message = req.body.message;
+        let conversationId = req.body.conversationId;
+
+        if (message === undefined) {
+            res.send({status:'missing message'})
+        };
+
+        if (conversationId === undefined) {
+            conversationId = new ObjectID();
+        };
+        const sender = new ObjectID();
+
+        db.collection('messages').insertOne({
+            from: sender,
+            to: 'bot',
+            content: message,
+            conversationId: conversationId
+        }, (err, r) => {
+            assert.equal(null, err);
+        });
+    });
+};
+
+function getMessages(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client(dbName);
+
+        db.collection('messages').find({}).toArray((err, docs) => {
+            assert.equal(null, err);
+            res.send(docs);
+        });
+    });
+};
+
+function getMessageById(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client(dbName);
+
+        db.collection('messages').find({id:req.params.id}).toArray((err, docs) => {
+            assert.equal(null, err);
+            res.send(docs);
+        });
+    });
+};
+
+function getMessagesInConversationId(req, res){
+    client.connect((err, client) => {
+        assert.equal(null, err);
+        const db = client(dbName);
+
+        db.collection('messages').find({conversationId: req.params.conversationId}).toArray((err, docs) => {
+            assert.equal(null, err);
+            res.send(docs);
+        });
+    });
+};
+
+app.get('/', (req, res) => {res.send('Hello World')});
+
+app.get('/bots', getBots);
+app.get('/bot/:id', getBotById);
+
+app.get('/messages', getMessages);
+app.post('/messages', postMessage);
+app.get('/messages/:id', getMessageById);
+app.get('/messages?conversationId=:conversationId', getMessagesInConversationId);
+
+app.listen(port, () => console.log('Assignment'))
